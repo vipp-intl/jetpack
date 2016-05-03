1dc48e7e815eec0e6f01aab21e5fb396a7b9c6c8
Jeremy Herve <jeremy@tagada.hu>
2016-05-03 09:23:24 +0200

    Shortcodes: remove audio shortcode
    - It was not included since WordPress 3.6.
    - It was removed from the Media Extractor once already, in #257

--- modules/shortcodes.php
+++ modules/shortcodes.php
@@ -9,7 +9,7 @@
  * Requires Connection: No
  * Auto Activate: Yes
  * Module Tags: Photos and Videos, Social, Writing, Appearance
- * Additional Search Queries: shortcodes, shortcode, embeds, media, bandcamp, blip.tv, dailymotion, digg, facebook, flickr, google calendars, google maps, google+, polldaddy, recipe, recipes, scribd, slideshare, slideshow, slideshows, soundcloud, ted, twitter, vimeo, vine, youtube
+ * Additional Search Queries: shortcodes, shortcode, embeds, media, bandcamp, blip.tv, dailymotion, facebook, flickr, google calendars, google maps, google+, polldaddy, recipe, recipes, scribd, slideshare, slideshow, slideshows, soundcloud, ted, twitter, vimeo, vine, youtube
  */
 
 /**
@@ -59,31 +59,7 @@ function jetpack_load_shortcodes() {
 	$shortcode_includes = apply_filters( 'jetpack_shortcodes_to_include', $shortcode_includes );
 
 	foreach ( $shortcode_includes as $include ) {
-		if ( version_compare( $wp_version, '3.6-z', '>=' ) && stristr( $include, 'audio.php' ) ) {
-			continue;
-		}
-
 		include $include;
-	}
-}
-
-global $wp_version;
-
-if ( version_compare( $wp_version, '3.6-z', '>=' ) ) {
-	add_filter( 'shortcode_atts_audio', 'jetpack_audio_atts_handler', 10, 3 );
-
-	function jetpack_audio_atts_handler( $out, $pairs, $atts ) {
-		if( isset( $atts[0] ) )
-			$out['src'] = $atts[0];
-
-		return $out;
-	}
-
-	function jetpack_shortcode_get_audio_id( $atts ) {
-		if ( isset( $atts[ 0 ] ) )
-			return $atts[ 0 ];
-		else
-			return 0;
 	}
 }
 
